4c980076dc83f0ec8d9f134a82438f36e3efda3b
Werner gehrke Junior <werner.junior@hbsis.com.br>
2021-03-03 20:44:12 -0300

    tete

--- src/server.js
+++ src/server.js
@@ -1,2 +1,6 @@
 const app = require("./app");
-app.listen(process.env.PORT);
+
+var port = process.env.PORT || 8080;
+
+app.listen(port,function() {
+  console.log(`app running on port ${port}`); });
